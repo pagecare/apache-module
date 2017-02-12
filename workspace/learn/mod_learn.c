86f7a8935c50c2d11c4388ee795ca6e5043e5baa
Jose <chous@acm-sl.org>
2017-02-12 16:47:52 +0100

    dev1.4

---
+++ workspace/learn/mod_learn.c
@@ -0,0 +1,46 @@
+/* Include the required headers from httpd */
+#include "httpd.h"
+#include "http_core.h"
+#include "http_protocol.h"
+#include "http_request.h"
+#include "http_config.h"
+
+/* Define prototypes of our functions in this module */
+static void register_hooks(apr_pool_t *pool);
+static int learn_handler(request_rec *r);
+
+/* Define our module as an entity and assign a function for registering hooks  */
+
+module AP_MODULE_DECLARE_DATA learn_module =
+{
+    STANDARD20_MODULE_STUFF,
+    NULL,            // Per-directory configuration handler
+    NULL,            // Merge handler for per-directory configurations
+    NULL,            // Per-server configuration handler
+    NULL,            // Merge handler for per-server configurations
+    NULL,            // Any directives we may have for httpd
+    register_hooks   // Our hook registering function
+};
+
+/* register_hooks: Adds a hook to the httpd process */
+static void register_hooks(apr_pool_t *pool) 
+{
+    /* Hook the request handler */
+    ap_hook_handler(learn_handler, NULL, NULL, APR_HOOK_LAST);
+}
+
+/* The handler function for our module.
+ * This is where all the fun happens!
+ */
+static int learn_handler(request_rec *r)
+{
+    /* First off, we need to check if this is a call for the "learn" handler.
+     * If it is, we accept it and do our things, it not, we simply return DECLINED,
+     * and Apache will try somewhere else.
+     */
+    if (!r->handler || strcmp(r->handler, "learn")) return (DECLINED);
+
+    // The first thing we will do is write a simple "Hello, world!" back to the client.
+    ap_rputs("Hello, world!", r);
+    return OK;
+}
